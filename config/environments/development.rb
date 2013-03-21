8004e7315b5f6727fc9a7be6bd07822060b5ccec
ShiningRay <tsowly@hotmail.com>
2013-03-21 17:36:58 +0800

    to support ehcache for jruby
    and add more docs for system_configuration.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,34 +4,16 @@
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
 config.cache_classes = false
-#config.log_level = :any
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false#true
+config.action_controller.perform_caching             = false
 config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :test
 #
-#config.gem 'memcached'
-#
-#config.cache_store = :libmemcached_store, "localhost:11211", {
-#  :buffer_requests => true,
-#}
 #config.cache_store = :mem_cache_store, "localhost:11211"
-#config.action_controller.session_store = :libmemcached_store
-#
-#Workling::Remote.dispatcher = Workling::Remote::Runners::SpawnRunner.new
-#Workling::Remote.invoker = Workling::Remote::Invokers::EventmachineSubscriber
-#Workling::Remote.dispatcher = Workling::Remote::Runners::ClientRunner.new
-#Workling::Remote.dispatcher.client = Workling::Clients::AmqpClient.new
-##
-#config.middleware.use ::Rack::Cache,
-#  :verbose => true,
-#  :metastore => 'file:/tmp/cache/rack/meta',
-#  :entitystore => 'file:/tmp/cache/rack/body'
-#config.gem 'awesome_print', :lib => 'ap'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,15 +15,8 @@ config.log_level = :error
 config.action_view.cache_template_loading = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
-config.action_controller.session_store = :active_record_store
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :sendmail
-
-begin
-require 'rack-cache' unless defined?(::Rack::Cache)
-rescue LoadError
-puts 'cannot load rack-cache'
-end
 
 path = Rails.root.join('config/system_configuration.rb')
 eval(IO.read(path), binding, path) if File.exists?(path)
