7fbc2f04b40a2e083fea4d4301a26e4f20c46ee3
ShiningRay <tsowly@hotmail.com>
2013-04-10 00:39:16 +0800

    fix background worker for thin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Moumentei::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :ehcache_store
+  # config.cache_store = :ehcache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
--- config/initializers/background_worker.rb
+++ config/initializers/background_worker.rb
@@ -1,8 +1,16 @@
 # -*- encoding : utf-8 -*-
-Thread.start do
-  EM.run {
-    EM.add_periodic_timer( 600 ) {
-      Article.recalc_alt_scores
-    }
-  }    
+timer = Proc.new {
+  EM.add_periodic_timer( 600 ) {
+    Article.recalc_alt_scores
+  }
+}
+Rails.application.config.after_initialize do
+  Thread.start do
+    sleep 300
+    if EM.reactor_running?
+      timer.call
+    else
+      EM.run &timer
+    end
+  end
 end
