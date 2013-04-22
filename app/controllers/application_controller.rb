94fb3686213e3cd07658dd5054101fd13270702c
ShiningRay <tsowly@hotmail.com>
2013-04-23 00:38:38 +0800

    don't redirect on domain mismatch unless configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < ActionController::Base
     #if request.host.index(group.inherited(:domain))
     #     redirect_to "#{request.protocol}#{request.host}#{(request.port == 80 ? '' : request.port_string)}#{request.path}#{(request.query_string.empty? ? '' : '?' + request.query_string )}"
     return unless group
+    return unless Setting.use_canonical_url
     domain = group.inherited(:domain)
     if request.get? and !domain.blank? and request.host != domain and request.format.html?
       redirect_to "#{request.protocol}#{domain}#{(request.port == 80 ? '' : request.port_string)}#{request.path}#{(request.query_string.empty? ? '' : '?' + request.query_string )}"
