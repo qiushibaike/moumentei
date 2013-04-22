6ca1ae24d4674718f80dd11c8c5ebe17197c70e0
ShiningRay <tsowly@hotmail.com>
2013-04-22 15:54:11 +0800

    make some output html safe

--- app/helpers/admin/groups_helper.rb
+++ app/helpers/admin/groups_helper.rb
@@ -2,7 +2,7 @@
 module Admin::GroupsHelper
   def options_check_box obj, field, label
     checked = "checked='check'" if @group.options[field.to_sym]
-    <<html
+    raw <<html
     <input type="hidden" name="group[options][#{field}]" value="no" />
     <label class='checkbox'><input type="checkbox" name="group[options][#{field}]" value="yes" #{checked} />
     #{label}</label>
