75aabe5b08c06a6757e9332cbf818583d93bb64c
ShiningRay <tsowly@hotmail.com>
2013-05-02 17:39:42 +0800

    travis & spec

--- spec/models/anonymous_rating_spec.rb
+++ spec/models/anonymous_rating_spec.rb
@@ -4,10 +4,16 @@ describe AnonymousRating do
   let(:article){ create :article }
   let(:user){ create :user }
 
-  it 'should do something' do
+  it 'should be able to rate and not rate again' do
     lambda{
       AnonymousRating.vote('127.0.0.1', article, 1)
     }.should change(AnonymousRating, :count).by(1)
+    article.reload
+    article.score.should == 1
+    article.pos.should == 1
+    lambda{
+      AnonymousRating.vote('127.0.0.1', article, 1)
+    }.should_not change(AnonymousRating, :count)
     article.reload
     article.score.should == 1
     article.pos.should == 1
