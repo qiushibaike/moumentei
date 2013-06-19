076743eb72192b94b0fe8557be5ee0fc0a80918d
ShiningRay <tsowly@hotmail.com>
2013-06-20 00:23:25 +0800

    can show pages from other groups

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 module PagesHelper
-  def show_page(path)
-    if page = @group.pages.find_by_path(path)
+  def show_page(path, group=@group)
+    if page = group.pages.find_by_path(path)
       raw(page.content)
     else
       raw("<!-- cannot find page of '#{path}' -->")
