6ebaf43527ee75553005dcdce159089e65adf996
ShiningRay <tsowly@hotmail.com>
2014-06-26 15:26:56 +0800

    move out some configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,6 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'tearoom'
-set :repo_url, 'root@106.186.125.95:/srv/repos/tearoom.git/'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :branch, 'master'
-
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/srv/#{fetch(:application)}"
-
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -29,6 +16,8 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
+# local_file = File.expand_path("../deploy_local.rb", __FILE__)
+# eval(IO.read(local_file), binding, local_file) if File.exists?(local_file)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
